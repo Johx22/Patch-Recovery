5ce27296bfe343783ffa2322041d3f92716bce9d
Johx22 <johannjose2004@gmail.com>
2022-04-19 14:46:06 +0300

    Upload files

---
+++ script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
+dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes 
+
+if [ ! -f phh.pem ];then
+    openssl genrsa -f4 -out phh.pem 4096
+fi
---
+++ script2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir unpack
+cd unpack
+../magiskboot unpack ../r.img
+../magiskboot cpio ramdisk.cpio extract
+# Reverse fastbootd ENG mode check
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 # 20 01 00 35
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 50c0009420010034 50c0009420010035
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 080109aae80000b4 080109aae80000b5
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 20f0a6ef38b1681c 20f0a6ef38b9681c
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 23f03aed38b1681c 23f03aed38b9681c
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 20f09eef38b1681c 20f09eef38b9681c
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 26f0ceec30b1681c 26f0ceec30b9681c
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot  cpio ramdisk.cpio 'add 0755 system/bin/recovery system/bin/recovery'
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot  repack ../r.img new-boot.img
+cp new-boot.img ../recovery-patched.img
