e4ccdb03a5cc7f390e02da4c2c1a5374e0b414c9
MrWicked <therealmrwicked@gmail.com>
2023-12-26 17:15:18 -0400

    Fixed an issue with the patching process on windows

--- patchrecovery.bat
+++ patchrecovery.bat
@@ -42,20 +42,29 @@ cd temp || exit /b
 
 ..\magiskboot unpack ../recovery.img
 ..\magiskboot cpio ramdisk.cpio extract
-..\magiskboot hexpatch ramdisk/system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094
-..\magiskboot hexpatch ramdisk/system/bin/recovery eec3009420010034 eec3009420010035
-..\magiskboot hexpatch ramdisk/system/bin/recovery 3ad3009420010034 3ad3009420010035
-..\magiskboot hexpatch ramdisk/system/bin/recovery 50c0009420010034 50c0009420010035
-..\magiskboot hexpatch ramdisk/system/bin/recovery 080109aae80000b4 080109aae80000b5
-..\magiskboot hexpatch ramdisk/system/bin/recovery 20f0a6ef38b1681c 20f0a6ef38b9681c
-..\magiskboot hexpatch ramdisk/system/bin/recovery 23f03aed38b1681c 23f03aed38b9681c
-..\magiskboot hexpatch ramdisk/system/bin/recovery 20f09eef38b1681c 20f09eef38b9681c
-..\magiskboot hexpatch ramdisk/system/bin/recovery 26f0ceec30b1681c 26f0ceec30b9681c
-..\magiskboot hexpatch ramdisk/system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
-..\magiskboot hexpatch ramdisk/system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
-..\magiskboot hexpatch ramdisk/system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
-..\magiskboot hexpatch ramdisk/system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
-..\magiskboot cpio ramdisk.cpio "add 0755 ramdisk/system/bin/recovery ramdisk/system/bin/recovery"
+
+cd ramdisk || exit /b
+for %%a in (*) do move "%%a" ..
+for /d %%a in (*) do move "%%a" ..
+cd .. || exit /b
+
+del cpio
+rmdir /S /Q ramdisk
+
+..\magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094
+..\magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
+..\magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
+..\magiskboot hexpatch system/bin/recovery 50c0009420010034 50c0009420010035
+..\magiskboot hexpatch system/bin/recovery 080109aae80000b4 080109aae80000b5
+..\magiskboot hexpatch system/bin/recovery 20f0a6ef38b1681c 20f0a6ef38b9681c
+..\magiskboot hexpatch system/bin/recovery 23f03aed38b1681c 23f03aed38b9681c
+..\magiskboot hexpatch system/bin/recovery 20f09eef38b1681c 20f09eef38b9681c
+..\magiskboot hexpatch system/bin/recovery 26f0ceec30b1681c 26f0ceec30b9681c
+..\magiskboot hexpatch system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
+..\magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
+..\magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
+..\magiskboot hexpatch system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
+..\magiskboot cpio ramdisk.cpio "add 0755 system/bin/recovery system/bin/recovery"
 ..\magiskboot repack ../recovery.img ../recovery-patched.img
 
 cd.. || exit /b
