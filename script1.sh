7e6564c3492b64023f0326f6683222bf41d7dd20
C-F0x <132144679+C-F0x@users.noreply.github.com>
2024-06-21 01:51:56 +0800

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -f recovery.img.lz4 ];then
-	lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
+if [ -f *.img.lz4 ];then
+	lz4 -B6 --content-size -f *.img.lz4 recovery.img
 fi
 
 off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
-dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
+dd if=*.img of=r.img bs=4k count=$off iflag=count_bytes
 
 if [ ! -f phh.pem ];then
     openssl genrsa -f4 -out phh.pem 4096
